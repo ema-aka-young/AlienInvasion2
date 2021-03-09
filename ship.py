e3f075e24f7a1a2f737b5b256733a80ddaa781bb
Emanuele Prella <54805081+ema-aka-young@users.noreply.github.com>
2021-03-09 13:31:06 +0100

    first classes and movements

---
+++ ship.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Ship():
+
+    def __init__(self, ai_settings, screen):
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        #Load ship image
+        self.image = pygame.image.load("images\ship.bmp")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #new ship at the bottom center of the screen
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        #store decimal value for ship's center
+        self.center = float(self.rect.centerx)
+
+        #Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        #update ship position based on movement flags
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0 :
+            self.center -= self.ai_settings.ship_speed_factor
+
+        #update rect object from self.center
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        #draw ship
+        self.screen.blit(self.image, self.rect)
